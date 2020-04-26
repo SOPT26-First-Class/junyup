87d73bf0f01e7826297674110017efaf3d8722db
JunyupHong <junyup.hong@gmail.com>
2020-04-26 14:34:07 +0900

    코드 수정

--- week2/password.js
+++ week2/password.js
@@ -1,18 +1,19 @@
 const fs = require('fs');
 const crypto = require('crypto');
 
+(async () => {
+    try{
+        const password = await fs.readFileSync('./password.txt').toString();
+        const salt = crypto.randomBytes(32).toString('hex')        
+        const derivedKey = await crypto.pbkdf2Sync(password, salt, 1, 32, 'sha512');
+        const hashed = derivedKey.toString('hex');
+        await fs.writeFileSync('./hashed.txt', `${salt}\n${hashed}`);
+        console.log('salt : ', salt);
+        console.log('hashed : ', hashed);
+        console.log('saved!')
+    } catch(e) {
+        console.error(e);
+    }
+})();
 
-const password = fs.readFileSync('./password.txt').toString();
-console.log(password);
 
-const encrypt = (salt, password) => {
-    crypto.pbkdf2(password, salt, 1, 32, 'sha512', async (err, derivedKey) => {
-            if (err) throw err;
-            const hashed = derivedKey.toString('hex');
-            await fs.writeFileSync('./hashed.txt', `${salt}\n${hashed}`);
-            console.log('salt : ', salt);
-            console.log('hashed : ', hashed);
-        });
-    };
-
-encrypt(crypto.randomBytes(32).toString('hex'), password);
