b89d01894f7f2314fd58735d306e9247a4318da9
JunyupHong <junyup.hong@gmail.com>
2020-05-09 21:51:20 +0900

    update

--- week2/password.js
+++ week2/password.js
@@ -1,16 +1,21 @@
 const fs = require('fs');
 const crypto = require('crypto');
 
+const getSalt = (length) => {
+    return crypto.randomBytes(length).toString('hex');
+};
+
 (async () => {
     try{
         const password = await fs.readFileSync('./password.txt').toString();
-        const salt = crypto.randomBytes(32).toString('hex')        
-        const derivedKey = await crypto.pbkdf2Sync(password, salt, 1, 32, 'sha512');
-        const hashed = derivedKey.toString('hex');
+        const salt = getSalt(32);
+        const hashed = (await crypto.pbkdf2Sync(password, salt, 1, 32, 'sha512')).toString('hex');
+        
         await fs.writeFileSync('./hashed.txt', `${salt}\n${hashed}`);
+        
         console.log('salt : ', salt);
         console.log('hashed : ', hashed);
-        console.log('saved!')
+        console.log('saved!');
     } catch(e) {
         console.error(e);
     }
